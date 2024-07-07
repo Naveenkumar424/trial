86c1e194d59001c18f3c0063bee8139894b63d23
Naveen <naveenkulaljs2@gmail.com>
2024-07-07 11:03:21 +0530

    Alterations

---
+++ script.js
@@ -0,0 +1,9 @@
+function display(){
+    let d = document.querySelector("#anu");
+    d.style.display = "flex"
+}
+
+function noDisplay(){
+    let d = document.querySelector("#anu");
+    d.style.display = "none"
+}
